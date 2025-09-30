d7df351438515f27351afea1b9e03ce33eceed5a
julia-daniluski <julia.o.daniluski@gmail.com>
2025-09-30 14:06:19 -0300

    arrumei

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ $alerta_estoque = gerarAlertaEstoque($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Pastelaria</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon"> 
 </head>
 <body>
     <div class="container">
