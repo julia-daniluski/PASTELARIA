db5195be0b160a0abf972a6b28dbc55577d76196
julia-daniluski <julia.o.daniluski@gmail.com>
2025-09-30 10:07:37 -0300

    favicon

--- login.php
+++ login.php
@@ -45,6 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Sistema Pastelaria</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon"> 
 </head>
 <body class="login-page">
     <div class="login-container">
